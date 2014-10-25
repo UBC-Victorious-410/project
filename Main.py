b6731401525cd1cb169c19747765817e2b712431
William Hsiao <willy40702@gmail.com>
2014-10-25 16:56:12 -0700

    updated Main.py with 1. generate gitLog.txt from git log --stat 2. mock PMD execution

--- Main.py
+++ Main.py
@@ -1,49 +1,70 @@
 from web import visualizer
 import sys
 import os
+import shutil
 from git import *
 
 
 def main(argv):
     opt = argv[0]
     if opt == "getRepo":
-        if not os.path.isdir("./Target"):
-            os.mkdir("Target")
-        Repo.clone_from(argv[1], "./Target")
+        try:
+            git_pull(argv[1])
+        except Exception, e:
+            print (str(e))
     elif opt == "gitLog":
-        
-        print "do git log"
+        git_log()
     elif opt == "PMD":
-        print "do PMD"
+        PMD()
     elif opt == "Parse":
         print "do parse"
     elif opt == "GenerateJSON":
         print "do GenerateJSON"
     elif opt == "GenerateGraph":
         visualizer.begin()
-
-
-
     else:
         usage()
 
-def PMD(argv):
 
-    print "do PMD"
+
+def git_pull(git_dir):
+    if not os.path.isdir("./Target"):
+        os.mkdir("Target")
+        Repo.clone_from(git_dir, "./Target")
+    else:
+        print "/Target folder exist, please remove it to get new repository"
+
+def git_log():
+    g = Git('./Target/')
+    gitLog = g.log("--reverse","--stat")
+    with open('./PMDResult/gitLog.txt','w') as r:
+        r.write(gitLog)
+    print ("gitLog.txt is placed in PMDResult")
+
+def PMD():
+    g = Git('./Target/')
+    commits = g.log("--reverse", "--pretty=%h").split("\n")
+    for c in commits:
+        print ("git checkout -b " + c)
+        print ("Execute PMD on commit: " + c)
+
 
 
 
 def usage():
     print "Usage: Main.py <option> "
-    print "option  :      getRepo <git repo address> - perform 'git pull' with given repoURL at ./target"
-    print "               gitLog                     - generate gitLog.xml in PMDResult"
-    print "               PMD                        - analyze target/ with PMD. place xml results in PMDResults"
-    print "               Parse                      - execute parsers to parse result within PMDResults"
-    print "               GenerateJSON               - generate JSON from the fuser "
-    print "               GenerateGraph              - generate the result with D3 using JSON"
-
+    print "option: getRepo <git repo address> - perform 'git pull' with given repoURL at ./target"
+    print "        gitLog                     - generate gitLog.xml in PMDResult"
+    print "        PMD                        - analyze target/ with PMD. place xml results in PMDResults"
+    print "        Parse                      - execute parsers to parse result within PMDResults"
+    print "        GenerateJSON               - generate JSON from the fuser "
+    print "        GenerateGraph              - generate the result with D3 using JSON"
+#
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    if len(sys.argv) == 1:
+        usage()
+    else:
+        main(sys.argv[1:])
 
 
 
