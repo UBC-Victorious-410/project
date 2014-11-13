b87ca8357b4a700bdfdf8588cfd4fba57ac215c1
austinl9 <acm.liu11@gmail.com>
2014-11-12 21:30:50 -0800

    austin's parser

---
+++ tools/pmd_parser.py
@@ -0,0 +1,62 @@
+__author__ = 'Austin'
+
+
+import os,os.path
+
+
+#get the path to the pmd commits
+#gwtcwd = get current working directory
+
+#print os.getcwd()
+
+
+rootdir = os.path.abspath(os.path.dirname(os.getcwd()))
+#print rootdir
+
+pmd_folder = os.path.dirname(rootdir+"\PMDResult/")
+
+print pmd_folder
+
+#print os.listdir(pmd_folder)
+
+#print os.path.isfile(pmd_folder+"\commit2.xml")
+
+
+i = 0
+commit_result = []
+file_name = 'commit'
+
+
+if os.path.isfile(pmd_folder+"\\"+"CommitResult.txt") is False:
+    print "CommitResult doesn't exist"
+
+    completeName = os.path.join(pmd_folder, "CommitResult.txt")
+
+    file1 = open(completeName, "w")
+
+    for file in os.listdir(pmd_folder):
+        print file
+        num_viol = 0
+        i = i + 1
+
+        if os.path.isfile(pmd_folder +"\\"+ "commit"+str(i)+".xml"):
+            file1.write("commit"+str(i)+".xml")
+            f = open(pmd_folder +"\\"+ "commit"+str(i)+".xml")
+            lines = f.readlines()
+
+            for line in lines:
+                #print line
+
+                if '</violation>' in line:
+                    num_viol = num_viol + 1
+
+            commit_result.append(file)
+            commit_result.append(num_viol)
+            file1.write(" "+str(num_viol) +"\n")
+            print num_viol
+            f.close()
+
+    print commit_result
+    file1.close()
+elif os.path.isfile(pmd_folder+"\\"+"CommitResult.txt") is True:
+    print "CommitResult already exists!"
